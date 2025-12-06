e23d78ff261ff0e47c77c8fa7c1370530f31a9b3
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-06 18:26:57 +0530

    Update Functiondemo.c

--- Functiondemo.c
+++ Functiondemo.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    int Value1 = 0, Value2 = 0, Ans = 0;
+    int Value1 = 0, Value2 = 0, Ans = 0;   // Variables
 
-    Value1 = 10;
+    Value1 = 10;    // Assign values
     Value2 = 11;
 
-    Ans = Value1 + Value2;
+    Ans = Value1 + Value2;    // Addition
 
-    printf("Addition is : %d\n",Ans);
+    printf("Addition is : %d\n",Ans);   // Print result
 
     return 0;
 }
