b56b5894f4174f2973fdbed532926895211d1638
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-06 15:27:15 +0530

    Update Functiondemo.c

--- Functiondemo.c
+++ Functiondemo.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>   // Header file for input/output functions
 
+// Main function - entry point of the program
 int main()
 {
+    // Declare integer variables and initialize to 0
     int Value1 = 0, Value2 = 0, Ans = 0;
 
+    // Assign values to the variables
     Value1 = 10;
     Value2 = 11;
 
+    // Perform addition and store result in Ans
     Ans = Value1 + Value2;
 
-    printf("Addition is : %d\n",Ans);
+    // Print the result to the console
+    printf("Addition is : %d\n", Ans);
 
+    // Return 0 to indicate successful execution
     return 0;
 }
