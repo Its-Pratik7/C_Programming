0b8f2fdaeaf32df7e9e8ae12e75780f25bffc7ed
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-06 18:40:03 +0530

    Update FunctionIo.c

--- FunctionIo.c
+++ FunctionIo.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-    int iValue1 = 0, iValue2 = 0, iAns = 0;
+    int iValue1 = 0, iValue2 = 0, iAns = 0; // Variables
 
     printf("Enter first number : \n");
-    scanf("%d",&iValue1);
+    scanf("%d",&iValue1);   // Input 1
 
     printf("Enter second number : \n");
-    scanf("%d",&iValue2);
+    scanf("%d",&iValue2);   // Input 2
     
-    iAns = iValue1 + iValue2;
+    iAns = iValue1 + iValue2;   // Addition
 
-    printf("Addition is : %d\n",iAns);
+    printf("Addition is : %d\n",iAns); // Output
     
     return 0;
 }
