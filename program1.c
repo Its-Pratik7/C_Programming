404f58b0997e5076d519de04eabd2be6ff670732
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-06 15:39:37 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,11 +1,9 @@
-#include <stdio.h>       // Standard I/O header file
+#include <stdio.h>
 
-// Entry point function
 int main()
 {
-    // Display message on screen
     printf("jay ganesh...\n");
-
-    // Successful program termination
+    
     return 0;
 }
+
