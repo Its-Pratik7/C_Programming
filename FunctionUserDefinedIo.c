d7ab9b97734d92c7c4ecb46e9a91963df36b40f6
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-09 10:14:40 +0530

    Update FunctionUserDefinedIo.c

--- FunctionUserDefinedIo.c
+++ FunctionUserDefinedIo.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
+// Function to add two numbers
 int Addition(int iNo1, int iNo2)
 {
     int iSum = 0;
-    iSum = iNo1 + iNo2;
-    return iSum;
+    iSum = iNo1 + iNo2;   // Perform addition
+    return iSum;          // Return result
 }
 
 int main()
@@ -12,13 +13,13 @@ int main()
     int iValue1 = 0, iValue2 = 0, iAns = 0;
 
     printf("Enter first number : \n");
-    scanf("%d",&iValue1);
+    scanf("%d",&iValue1);   // Read first number
 
     printf("Enter second number : \n");
-    scanf("%d",&iValue2);
+    scanf("%d",&iValue2);   // Read second number
 
-    iAns = Addition(iValue1, iValue2);
-    printf("Addition is : %d\n",iAns);
+    iAns = Addition(iValue1, iValue2);   // Call addition function
+    printf("Addition is : %d\n",iAns);   // Display result
 
-    return 0;
+    return 0;   // Exit program
 }
