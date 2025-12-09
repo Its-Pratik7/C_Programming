0c83152782a565c8e5e73ff7365c34837014260a
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-09 12:03:07 +0530

    Update Shorthand.c

--- Shorthand.c
+++ Shorthand.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-    int i = 10, j = 10;
-    int iAns = 0, jAns = 0;
+    int i = 10, j = 10;        // initialize variables
+    int iAns = 0, jAns = 0;    // store results
 
-    iAns = i++;
-    jAns = ++j;
+    iAns = i++;    // post-increment: assign then increase
+    jAns = ++j;    // pre-increment: increase then assign
 
-    printf("Value of i : %d\n",i);          // 11
-    printf("Value of iAns : %d\n",iAns);    // 10
+    printf("Value of i : %d\n",i);          // i becomes 11
+    printf("Value of iAns : %d\n",iAns);    // stores old value 10
 
-    printf("Value of j : %d\n",j);          // 11
-    printf("Value of jAns : %d\n",jAns);    // 11
+    printf("Value of j : %d\n",j);          // j becomes 11
+    printf("Value of jAns : %d\n",jAns);    // stores new value 11
 
-    return 0;
+    return 0;   // end
 }
