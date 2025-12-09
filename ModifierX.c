59ff55f3885fe4d12cfca1134df7750ce85bfb61
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-09 12:10:42 +0530

    Update ModifierX.c

--- ModifierX.c
+++ ModifierX.c
@@ -2,21 +2,21 @@
 
 int main()
 {
-    int i = 10;
-    int j = -10;
-    int k = +10;
+    int i = 10;        // normal int
+    int j = -10;       // negative value
+    int k = +10;       // positive value
 
-    signed int a = 10;
-    signed int b = -10;
-    signed int c = +10;
+    signed int a = 10;     // signed integer (default)
+    signed int b = -10;    // signed negative value
+    signed int c = +10;    // signed positive value
 
-    unsigned int x = 10;
-    unsigned int y = +10;
-    unsigned int z = -10;
+    unsigned int x = 10;   // unsigned positive
+    unsigned int y = +10;  // unsigned positive
+    unsigned int z = -10;  // invalid but stored as large unsigned value
 
-    printf("%d\n",x);
-    printf("%d\n",y);
-    printf("%d\n",z);
+    printf("%d\n",x);   // print unsigned int
+    printf("%d\n",y);   // print unsigned int
+    printf("%d\n",z);   // prints wrapped value due to unsigned overflow
     
-    return 0;
+    return 0;   // end
 }
