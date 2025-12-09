2ad32d11e8841a6d38a4a2fcd2f656d79b40081f
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-09 12:08:10 +0530

    Update Modifier.c

--- Modifier.c
+++ Modifier.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int i = 10;
-    short int j = 20;
-    long int k = 30;
+    int i = 10;           // default integer
+    short int j = 20;     // short type integer
+    long int k = 30;      // long type integer
 
-    printf("size of integer is : %lu\n",sizeof(i));         // 4
-    printf("size of short integer is : %lu\n",sizeof(j));   // 2
-    printf("size of long integer is : %lu\n",sizeof(k));    // 8
+    printf("size of integer is : %lu\n",sizeof(i));         // default int size
+    printf("size of short integer is : %lu\n",sizeof(j));   // short int size
+    printf("size of long integer is : %lu\n",sizeof(k));    // long int size
 
-    return 0;
+    return 0;   // end
 }
