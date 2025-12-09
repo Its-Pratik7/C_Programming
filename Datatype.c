cf7e5343053c3f34cc9edc6888881beca49f5d95
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-09 10:35:00 +0530

    Update Datatype.c

--- Datatype.c
+++ Datatype.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-    char cValue = 'S';
-    int iValue = 11;
-    float fValue = 90.78f;
-    double dValue = 98.564323;
+    char cValue = 'S';          // character variable
+    int iValue = 11;            // integer variable
+    float fValue = 90.78f;      // float variable
+    double dValue = 98.564323;  // double variable
 
-    printf("%c\n",cValue);
-    printf("%d\n",iValue);
-    printf("%f\n",fValue);
-    printf("%lf\n",dValue);
+    printf("%c\n",cValue);      // print char
+    printf("%d\n",iValue);      // print int
+    printf("%f\n",fValue);      // print float
+    printf("%lf\n",dValue);     // print double
     
-    return 0;
+    return 0;                   // return from main
 }
