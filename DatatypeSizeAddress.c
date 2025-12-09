7834d88355a6a674b0675c8ab342654ba2c3c424
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-09 12:01:12 +0530

    Update DatatypeSizeAddress.c

--- DatatypeSizeAddress.c
+++ DatatypeSizeAddress.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
-int iMarks = 89;
+int iMarks = 89;     // global variable
 
 int main()
 {
-    char cValue = 'S';
-    int iValue = 11;
-    float fValue = 90.78f;
-    double dValue = 98.564323;
+    char cValue = 'S';          // character variable
+    int iValue = 11;            // integer variable
+    float fValue = 90.78f;      // float variable
+    double dValue = 98.564323;  // double variable
 
-    printf("Size of character is : %lu\n",sizeof(cValue));
-    printf("Size of integer is : %lu\n",sizeof(iValue));
-    printf("Size of flaot is : %lu\n",sizeof(fValue));
-    printf("Size of double is : %lu\n",sizeof(dValue));
+    printf("Size of character is : %lu\n",sizeof(cValue));   // print char size
+    printf("Size of integer is : %lu\n",sizeof(iValue));     // print int size
+    printf("Size of flaot is : %lu\n",sizeof(fValue));       // print float size
+    printf("Size of double is : %lu\n",sizeof(dValue));      // print double size
     
-    printf("Address of cValue is : %lu\n",&cValue);
-    printf("Address of iValue is : %lu\n",&iValue);
-    printf("Address of fValue is : %lu\n",&fValue);
-    printf("Address of dValue is : %lu\n",&dValue);
+    printf("Address of cValue is : %lu\n",&cValue);   // print memory address
+    printf("Address of iValue is : %lu\n",&iValue);   // print memory address
+    printf("Address of fValue is : %lu\n",&fValue);   // print memory address
+    printf("Address of dValue is : %lu\n",&dValue);   // print memory address
 
-    return 0;
+    return 0;   // end of program
 }
