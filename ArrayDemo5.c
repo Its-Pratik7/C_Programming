0eb1561076e2da730000d59e7cb9c6a5a4caac7b
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-10 16:04:52 +0530

    Update ArrayDemo5.c

--- ArrayDemo5.c
+++ ArrayDemo5.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    double Arr[] = {10.0,20.0,30.0,40.0};
+    double Arr[] = {10.0,20.0,30.0,40.0};   // double array
 
-    printf("Size of Arr : %lu : \n",sizeof(Arr));   // 32
+    printf("Size of Arr : %lu : \n",sizeof(Arr));   // total size (4 * 8 bytes = 32)
 
-    Arr++;      // Error
+    Arr++;      // Error: array name is not modifiable (cannot increment)
 
     return 0;
 }
