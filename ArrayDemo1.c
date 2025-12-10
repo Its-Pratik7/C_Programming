a96c4c8d2d511d807e4a2e13bc3a8e9b6d366ad4
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-10 15:57:09 +0530

    Update ArrayDemo1.c

--- ArrayDemo1.c
+++ ArrayDemo1.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int Arr[4] = {10,20,30,40};
-    int Brr[] = {10,20,30,40,50};
-    int Crr[6] = {10,20,30};
+    int Arr[4] = {10,20,30,40};      // declaring array with 4 elements
+    int Brr[] = {10,20,30,40,50};    // array size decided by elements
+    int Crr[6] = {10,20,30};         // remaining elements auto-filled with 0
 
-    printf("Size of Arr is : %lu\n",sizeof(Arr)); // 16
-    printf("Size of Brr is : %lu\n",sizeof(Brr)); // 20
-    printf("Size of Crr is : %lu\n",sizeof(Crr)); // 24
+    printf("Size of Arr is : %lu\n",sizeof(Arr)); // prints size of Arr (16)
+    printf("Size of Brr is : %lu\n",sizeof(Brr)); // prints size of Brr (20)
+    printf("Size of Crr is : %lu\n",sizeof(Crr)); // prints size of Crr  (24)
 
     return 0;
 }
