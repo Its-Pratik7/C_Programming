dc77676051b5f7589ae89eaf848ee1d108578c2f
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-10 16:03:52 +0530

    Update ArrayDemo4.c

--- ArrayDemo4.c
+++ ArrayDemo4.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-    int Arr[5] = {10,20,30,40,50};
+    int Arr[5] = {10,20,30,40,50};   // array with 5 elements
 
-    printf("Size of Arr : %lu\n",sizeof(Arr));  // 20
+    printf("Size of Arr : %lu\n",sizeof(Arr));  // total size of array in bytes
     
-    printf("Arr : %lu\n",Arr);              // 100
-    printf("&Arr : %lu\n",&Arr);            // 100
+    printf("Arr : %lu\n",Arr);        // base address of array
+    printf("&Arr : %lu\n",&Arr);      // address of entire array
     
-    printf("Arr + 1 : %lu\n",Arr + 1);      // 104
-    printf("(&Arr) + 1 : %lu\n",(&Arr) + 1);    // 120
+    printf("Arr + 1 : %lu\n",Arr + 1);        // moves by size of int (4 bytes)
+    printf("(&Arr) + 1 : %lu\n",(&Arr) + 1);  // moves by full array size (20 bytes)
 
     return 0;
 }
