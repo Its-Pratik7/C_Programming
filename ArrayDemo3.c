fd0582ae0faf098ce232fb93053653037f51d120
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-10 16:01:53 +0530

    Update ArrayDemo3.c

--- ArrayDemo3.c
+++ ArrayDemo3.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    int Arr[4] = {10,20,30,40};
+    int Arr[4] = {10,20,30,40};   // simple integer array
 
-    printf("Arr : %lu\n",Arr);                  // 100
-    printf("&Arr : %lu\n",&Arr);                // 100
-    printf("&(Arr[0]) : %lu\n",&(Arr[0]));      // 100
+    printf("Arr : %lu\n",Arr);                // prints base address     All three prints same address
+    printf("&Arr : %lu\n",&Arr);              // prints address of array
+    printf("&(Arr[0]) : %lu\n",&(Arr[0]));    // prints address of first element
 
     return 0;
 }
