b1ea89b9b10e071b2790b6d45e72cc8689e474b4
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-10 15:59:32 +0530

    Update ArrayDemo2.c

--- ArrayDemo2.c
+++ ArrayDemo2.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int Arr[4] = {10,20,30,40};
+    int Arr[4] = {10,20,30,40};   // array initialization
     
-    int Brr[3];
-    Brr[2] = 30;
+    int Brr[3];                   // array declaration without values
+    Brr[2] = 30;                  // assigning values manually
     Brr[0] = 10;
     Brr[1] = 20;
 
-    printf("%d\n",Arr[0]);      // 10
-    printf("%d\n",Arr[1]);      // 20
+    printf("%d\n",Arr[0]);        // printing first element
+    printf("%d\n",Arr[1]);        // printing second element
 
-    printf("%d\n",Brr[0]);      // 10
-    printf("%d\n",Brr[1]);      // 20
+    printf("%d\n",Brr[0]);        // printing Brr first element
+    printf("%d\n",Brr[1]);        // printing Brr second element
 
     return 0;
 }
