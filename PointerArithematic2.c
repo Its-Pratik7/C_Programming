6dbcbc764bbbd6a735313bbc9329325ab07df093
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-11 11:53:53 +0530

    Update PointerArithematic2.c

--- PointerArithematic2.c
+++ PointerArithematic2.c
@@ -2,22 +2,22 @@
 
 int main()
 {
-    int Arr[] = {11,21,51,101,111};
+    int Arr[] = {11,21,51,101,111};   // array
 
-    int *p = NULL;
-    int *q = NULL;
+    int *p = NULL;    // pointer p
+    int *q = NULL;    // pointer q
 
-    p = &(Arr[1]);
-    q = &(Arr[4]);
+    p = &(Arr[1]);    // p at 21
+    q = &(Arr[4]);    // q at 111
 
-    printf("Data fetched by p is : %d\n",*p);   // 21
-    printf("Data fetched by q is : %d\n",*q);   // 111
+    printf("Data fetched by p is : %d\n",*p);   // print p
+    printf("Data fetched by q is : %d\n",*q);   // print q
     
-    p++;
-    q--;
+    p++;              // move forward
+    q--;              // move backward
 
-    printf("Data fetched by p is : %d\n",*p);   // 51
-    printf("Data fetched by q is : %d\n",*q);   // 101
+    printf("Data fetched by p is : %d\n",*p);   // print new p
+    printf("Data fetched by q is : %d\n",*q);   // print new q
     
     return 0;
 }
