cb88ee646e7a41a1eccc983133a485e53c12dda4
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-11 11:56:34 +0530

    Update PointerArithematic4.c

--- PointerArithematic4.c
+++ PointerArithematic4.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-    int Arr[] = {11,21,51,101,111};
+    int Arr[] = {11,21,51,101,111};   // array
 
-    int *p = NULL;
-    int *q = NULL;
+    int *p = NULL;    // pointer p
+    int *q = NULL;    // pointer q
 
-    p = &(Arr[1]);
-    q = &(Arr[4]);
+    p = &(Arr[1]);    // p at 21
+    q = &(Arr[4]);    // q at 111
 
-    printf("Result of substraction is : %ld\n",q-p); // Allowed
+    printf("Result of substraction is : %ld\n",q-p);   // pointer diff
 
-    q = q - 2;                                       // Allowed
+    q = q - 2;        // move q
     
-    printf("Data pointed by q is %d\n",*q);
+    printf("Data pointed by q is %d\n",*q);    // print q value
  
     return 0;
 }
