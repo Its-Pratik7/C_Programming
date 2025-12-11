d9dfb75ace191f313abbe6e7df452051c179f304
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-11 11:54:43 +0530

    Update PointerArithematic3.c

--- PointerArithematic3.c
+++ PointerArithematic3.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int Arr[] = {11,21,51,101,111};
+    int Arr[] = {11,21,51,101,111};   // array
 
-    int *p = NULL;
-    int *q = NULL;
+    int *p = NULL;    // pointer p
+    int *q = NULL;    // pointer q
 
-    p = &(Arr[1]);
-    q = &(Arr[4]);
+    p = &(Arr[1]);    // p at 21
+    q = &(Arr[4]);    // q at 111
 
-    //p + q;      // Not allowed
-    p = p + 3;      // Allowed
+    //p + q;          // invalid
+    p = p + 3;        // move pointer
 
-    printf("Data pointed by p is : %d\n",*p);
+    printf("Data pointed by p is : %d\n",*p);   // print value
 
     return 0;
 }
