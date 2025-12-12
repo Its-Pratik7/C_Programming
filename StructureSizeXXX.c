2851e4b154a4fe4145466248db5147c11d86eee6
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-12 18:30:31 +0530

    Update StructureSizeXXX.c

--- StructureSizeXXX.c
+++ StructureSizeXXX.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 
-#pragma pack(1)
+#pragma pack(1)   
+// remove padding, pack members tightly
 struct Demo
 {
-    int i;
-    char ch1;
-    float f;
-    char ch2;
-    int j;
+    int i;        // int
+    char ch1;     // char
+    float f;      // float
+    char ch2;     // char
+    int j;        // int
 };
 
 int main()
 {
-    printf("Size of structure is : %lu\n",sizeof(struct Demo));
+    printf("Size of structure is : %lu\n",sizeof(struct Demo));   // print packed struct size
     
     return 0;
 }
