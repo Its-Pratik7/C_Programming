ce3cf71ce03acd4db597d51b1c30bebbc0ec2535
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-12 18:27:32 +0530

    Update StructureSize.c

--- StructureSize.c
+++ StructureSize.c
@@ -2,15 +2,15 @@
 
 struct Demo
 {
-    int i;
-    char ch;
-    float f;
-    int j;
+    int i;      // int (4 bytes)
+    char ch;    // char (1 byte)
+    float f;    // float (4 bytes)
+    int j;      // int (4 bytes)
 };
 
 int main()
 {
-    printf("Size of structure is : %lu\n",sizeof(struct Demo));
+    printf("Size of structure is : %lu\n",sizeof(struct Demo));   // print structure size
     
     return 0;
 }
