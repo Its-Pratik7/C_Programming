5a4a4a0636eb38b11b2bbeed580f8e6c3c272561
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-17 15:19:02 +0530

    Update Structure3.c

--- Structure3.c
+++ Structure3.c
@@ -1,29 +1,24 @@
-#include<stdio.h>
+#include<stdio.h>   // Standard I/O library
 
 struct Demo
 {
-    int i;
-    struct Demo * ptr;   
+    int i;          // Integer data
+    struct Demo * ptr;   // Pointer to same structure
 };
 
 int main()
-    {
-        struct Demo obj1;
-        struct Demo obj2;
-        struct Demo obj3;
+{
+    struct Demo obj1;    // First object
+    struct Demo obj2;    // Second object
+    struct Demo obj3;    // Third object
         
-        obj1.i = 11;
-        obj2.i = 21;
-        obj3.i = 51;
+    obj1.i = 11;         // Assign value to obj1
+    obj2.i = 21;         // Assign value to obj2
+    obj3.i = 51;         // Assign value to obj3
 
-        obj1.ptr = &obj2;
-        obj2.ptr = &obj3;
-        obj3.ptr = NULL;
-
+    obj1.ptr = &obj2;    // obj1 points to obj2
+    obj2.ptr = &obj3;    // obj2 points to obj3
+    obj3.ptr = NULL;     // obj3 points to nothing
         
-        
-    return 0;
-
-
-
-    }
+    return 0;            // End of program
+}
