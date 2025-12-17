1cdcd8e3de212fb98873d0f524c974dd668a5dd0
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-17 15:17:39 +0530

    Update Structure1.c

--- Structure1.c
+++ Structure1.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
+#include<stdio.h>   // Standard I/O library
 
 struct Demo
 {
-    int i;
-    float f;
-    struct Demo dobj;   // NA
+    int i;          // Integer member
+    float f;        // Float member
+    struct Demo dobj;   // Invalid: structure contains itself
 };
 
 int main()
 {
 
-    return 0;
+    return 0;       // Program ends
 }
