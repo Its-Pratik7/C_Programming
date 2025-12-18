c16fbfde960bc0e8354b03c4f2bf68f2ee0ecb31
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-19 00:00:12 +0530

    Update Staticdemo.c

--- Staticdemo.c
+++ Staticdemo.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-int main()
+#include<stdio.h>        // Header file
+
+int main()               // Main function
 {
-    int Arr[10];
+    int Arr[10];         // Static array
 
-    printf("Static memory Allocation\n");
+    printf("Static memory Allocation\n"); // Message
 
-    printf("%d\n",sizeof(Arr));
+    printf("%d\n",sizeof(Arr)); // Array size
 
-    return 0;
+    return 0;            // Program end
 }
